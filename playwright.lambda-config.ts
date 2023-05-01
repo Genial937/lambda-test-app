afaa9e92905bee6e6887e4f1258055118ec365da
Teddy <odenyothadeus@gmail.com>
2023-05-01 13:54:46 +0300

    added global setup

---
+++ playwright.lambda-config.ts
@@ -0,0 +1,88 @@
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  timeout: 300000,
+  expect: { timeout: 200000 },
+  globalTimeout: 300000,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    // baseURL: 'http://127.0.0.1:3000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    // {
+    //   name: 'chromium',
+    //   use: { ...devices['Desktop Chrome'] },
+    // },
+
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+    {
+      name: "chrome:latest:MacOS Ventura@lambdatest",
+      use: {
+        actionTimeout: 300000,
+        navigationTimeout: 300000,
+
+        viewport: { width: 1920, height: 1080 },
+      },
+    },
+    {
+      name: "chrome:latest:Windows 11@lambdatest",
+      use: {
+        viewport: { width: 1280, height: 720 },
+      },
+    },
+    {
+      name: "MicrosoftEdge:90:MacOS Ventura@lambdatest",
+      use: {
+        ...devices["iPhone 12 Pro Max"],
+      },
+    },
+    {
+      name: "pw-firefox:latest:Windows 11@lambdatest",
+      use: {
+        viewport: { width: 1280, height: 720 },
+      },
+    },
+    {
+      name: "pw-webkit:latest:Windows 10@lambdatest",
+      use: {
+        viewport: { width: 1920, height: 1080 },
+      },
+    },
+  ],
+
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://127.0.0.1:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
+});
